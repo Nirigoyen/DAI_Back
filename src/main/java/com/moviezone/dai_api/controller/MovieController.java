d92c1d1485dd24e7da942dd1d7244298430d998a
Nirigoyen <irigoyen.nicolas2002@gmail.com>
2024-05-11 16:41:30 -0300

    Prueba de acceso a TMDB

--- src/main/java/com/moviezone/dai_api/controller/MovieController.java
+++ src/main/java/com/moviezone/dai_api/controller/MovieController.java
@@ -5,9 +5,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
 import okhttp3.Response;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,22 +22,26 @@ public class MovieController {
     @GetMapping(value = "/movies")
     public ResponseEntity<String> discover() throws IOException {
 
-        String API_URL = "https://mi-api.com/recursos";
+        String API_URL = "https://api.themoviedb.org/3/discover/movie";
 
 
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity(API_URL, String.class);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("accept", "application/json");
+        headers.add("Authorization","Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjUxODkzODQ4NDEwYWE4OThmMzQ5MTYwZjc4MjFkZiIsInN1YiI6IjY1ZmNkMzFjMzUyMGU4MDE2NWQ1MDZjZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.d_ivgbfrcNn_yuTciheY2saSHuS2F2a5ZAAZds_uGU8");
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
 
-            if (response.getStatusCodeValue() == 200) {
-                String datos = response.getBody();
-                return new ResponseEntity<String>(datos, HttpStatus.OK);
-            } else {
+        ResponseEntity<String> response = restTemplate.exchange(API_URL,HttpMethod.GET, entity, String.class);
 
-            }
-            return response;
+        if (response.getStatusCodeValue() == 200) {
+            String datos = response.getBody();
+            return new ResponseEntity<String>(datos, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<String>("Error en la petición", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
 
-
+}
 
 
 
@@ -55,6 +57,6 @@ public class MovieController {
 //
 //        Response response = client.newCall(request).execute();
 //        return response;
-    }
+//    }
 
-}
+
