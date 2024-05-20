cdc05b5069e28001720b58342a4586cbd58c83c2
Nirigoyen <irigoyen.nicolas2002@gmail.com>
2024-05-19 21:05:33 -0300

    Creacion de User Controller

---
+++ src/main/java/com/moviezone/dai_api/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.moviezone.dai_api.controller;
+
+
+import com.moviezone.dai_api.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private IUserService userServiceImplementation;
+
+    public ResponseEntity<?> createUser(String username, String name, String lastName, String email, String birthDate){
+
+        
+        return null;
+    }
+
+}
