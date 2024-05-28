a1a16dabc63220e12cd39bf4f63aeb7b22044931
Nirigoyen <irigoyen.nicolas2002@gmail.com>
2024-05-27 23:27:23 -0300

    Tiene errores

--- src/main/java/com/moviezone/dai_api/controller/UserController.java
+++ src/main/java/com/moviezone/dai_api/controller/UserController.java
@@ -5,10 +5,16 @@ import com.moviezone.dai_api.model.dto.UserDTO;
 import com.moviezone.dai_api.service.IUserService;
 import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 
 @RestController
 @RequestMapping("/v1/users")
@@ -30,17 +36,37 @@ public class UserController {
     }
 
     @PostMapping(value = "/images")
-    public ResponseEntity<?> modifyImage(@RequestParam("archivo") MultipartFile archivo){
-
-        String URL = "https://dai-obs.obs.la-south-2.myhuaweicloud.com";
-
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("accept", "application/json");
-        HttpEntity<String> entity = new HttpEntity<String>(headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(URL, HttpMethod.GET, entity, String.class);
-
+    public ResponseEntity<?> modifyImage(@RequestParam String key, @RequestParam("archivo") MultipartFile archivo){
+//        String foto = "https://lh3.googleusercontent.com/a/ACg8ocLqCYrKJba4wsLo9oZqHvPhvHv6m9yGDcynwpEBrTmT45Zw7Q=s96-c";
+//
+//        RestTemplate restTemplateGet = new RestTemplate();
+//        HttpHeaders headersGet = new HttpHeaders();
+//
+//        headersGet.add("accept", "multipart/form-data");
+//
+//
+//        HttpEntity<MultiValueMap<String, Object>> requestEntityGet = new HttpEntity<>(headersGet);
+//
+//        ResponseEntity<MultipartFile> fotobajada = restTemplateGet.exchange(foto, HttpMethod.GET, requestEntityGet, MultipartFile.class);
+//
+//        MultipartFile asubir = fotobajada.getBody();
+//        String URL = "https://dai-obs.obs.la-south-2.myhuaweicloud.com/";
+//
+//        RestTemplate restTemplate = new RestTemplate();
+//        HttpHeaders headers = new HttpHeaders();
+//
+//        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+//
+//
+//        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+//        body.add("key", key);
+//        body.add("file", asubir.getResource());
+//
+//        HttpEntity<MultiValueMap<String, Object>> requestEntityPost = new HttpEntity<>(body, headers);
+//
+//        ResponseEntity<String> response = restTemplate.exchange(URL, HttpMethod.POST, requestEntityPost, String.class);
+//
+//        return new  ResponseEntity<>(response, HttpStatus.OK);
         return null;
     }
 
