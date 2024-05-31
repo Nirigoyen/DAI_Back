b9368499eaa74cfb8a496b4c05486e3e0e799644
Máximo Fain <maximofain0@gmail.com>
2024-05-30 22:54:50 -0300

    Security login

--- src/main/java/com/moviezone/dai_api/config/SecurityConfig.java
+++ src/main/java/com/moviezone/dai_api/config/SecurityConfig.java
@@ -1,46 +1,49 @@
-//package com.moviezone.dai_api.config;
-//
-//import io.jsonwebtoken.SignatureAlgorithm;
-//import io.jsonwebtoken.security.Keys;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import com.moviezone.dai_api.config.JWTAuthFilter;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//import javax.crypto.SecretKey;
-//
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-//        http.authorizeHttpRequests(
-//                        (authz) -> authz.anyRequest().authenticated())
-//                .addFilterBefore(jwtAuth(), UsernamePasswordAuthenticationFilter.class)
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .cors(Customizer.withDefaults());
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public JWTAuthFilter jwtAuth(){
-//        return new JWTAuthFilter(secretKey());
-//    }
-//
-//    @Bean
-//    public SecretKey secretKey(){
-//        SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-//        return secretKey;
-//    }
-//
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer(){
-//        return (web -> web.ignoring().requestMatchers("v1/auths/login"));
-//    }
-//}
+package com.moviezone.dai_api.config;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.moviezone.dai_api.config.JWTAuthFilter;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.crypto.SecretKey;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(
+                        (authz) -> authz.anyRequest().authenticated())
+                .addFilterBefore(jwtAuth(), UsernamePasswordAuthenticationFilter.class)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public JWTAuthFilter jwtAuth(){
+        return new JWTAuthFilter(secretKey());
+    }
+
+    @Bean
+    public SecretKey secretKey(){
+        SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        return secretKey;
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web -> web.ignoring().requestMatchers("v1/auths/login"));
+    }
+}
--- src/main/java/com/moviezone/dai_api/controller/AuthenticationController.java
+++ src/main/java/com/moviezone/dai_api/controller/AuthenticationController.java
@@ -10,23 +10,29 @@ import com.moviezone.dai_api.service.IUserService;
 import com.obs.services.ObsClient;
 import com.obs.services.ObsConfiguration;
 import io.github.cdimascio.dotenv.Dotenv;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import javax.crypto.SecretKey;
 import java.io.ByteArrayInputStream;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/v1/auths")
 public class AuthenticationController {
-    private final int EXPIRATION_TIME = 864_000_000; // 10 days Y esto? xDD
+    //private final int EXPIRATION_TIME = 864_000_000; // 10 days Y esto? xDD
+    private final int EXPIRATION_TIME = 60 * 5; // 5 minutos
+
 
     @Autowired
     private IUserService userService;
-//    @Autowired
-//    private SecretKey secretKey;
+    @Autowired
+    private SecretKey secretKey;
 
     /*
     @PostMapping("/login")
@@ -55,39 +61,40 @@ public class AuthenticationController {
 
         final String GOOGLE_TOKEN_VALIDATION_URL = "https://oauth2.googleapis.com/tokeninfo?id_token=" + googleToken.getToken(); // Should change this https://developers.google.com/identity/sign-in/web/backend-auth?hl=es-419
 
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("accept", "application/json");
-        HttpEntity<String> entity = new HttpEntity<String>(headers);
+//        RestTemplate restTemplate = new RestTemplate();
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add("accept", "application/json");
+//        HttpEntity<String> entity = new HttpEntity<String>(headers);
+//
+//        ResponseEntity<String> response = restTemplate.exchange(GOOGLE_TOKEN_VALIDATION_URL, HttpMethod.GET, entity, String.class);
+//
+//        if (response.getStatusCode() != HttpStatus.OK)
+//            return new ResponseEntity<>("Invalid GoogleToken", HttpStatus.CONFLICT); // Si el token es invalido no devolver JWT
+//
+//        String data = response.getBody();
+//
+//        JsonParser parser = new JsonParser();
+//        JsonObject jsonObject = (JsonObject) parser.parse(data);
+//
+//        String userId = jsonObject.get("sub").getAsString();
+//
+//        if (userService.findUserById(userId) == null){ // Si El usuario no esta en nuestra BD registrarlo
+//            UserDTO user = createUserDTO(jsonObject);
+//
+//            userService.createUser(user);
+//        }
+        String givenNameTest = "TheMaxcraft1";
 
-        ResponseEntity<String> response = restTemplate.exchange(GOOGLE_TOKEN_VALIDATION_URL, HttpMethod.GET, entity, String.class);
-
-        if (response.getStatusCode() != HttpStatus.OK)
-            return new ResponseEntity<>("Invalid GoogleToken", HttpStatus.CONFLICT); // Si el token es invalido no devolver JWT
-
-        String data = response.getBody();
-
-        JsonParser parser = new JsonParser();
-        JsonObject jsonObject = (JsonObject) parser.parse(data);
-
-        String userId = jsonObject.get("sub").getAsString();
-
-        if (userService.findUserById(userId) == null){ // Si El usuario no esta en nuestra BD registrarlo
-            UserDTO user = createUserDTO(jsonObject);
-
-            userService.createUser(user);
-        }
-
-
-//        String token = Jwts.builder()
+        String token = Jwts.builder()
 //                .setSubject(jsonObject.get("given_name").toString())
-//                .setIssuedAt(new Date())
-//                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-//                .signWith(secretKey, SignatureAlgorithm.HS256)
-//                .compact();
-//        return new ResponseEntity<>(token, HttpStatus.OK);
+                .setSubject(givenNameTest)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+        return new ResponseEntity<>(token, HttpStatus.OK);
 
-        return new ResponseEntity<>(data, HttpStatus.OK);
+        //return new ResponseEntity<>(data, HttpStatus.OK);
 
     }
 
