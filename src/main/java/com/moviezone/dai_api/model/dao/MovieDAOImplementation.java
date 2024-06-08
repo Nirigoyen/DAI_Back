a367da17b9598381aa4ed39e05adb2e54f27b39f
Nirigoyen <irigoyen.nicolas2002@gmail.com>
2024-06-08 15:48:13 -0300

    Clean

--- src/main/java/com/moviezone/dai_api/model/dao/MovieDAOImplementation.java
+++ src/main/java/com/moviezone/dai_api/model/dao/MovieDAOImplementation.java
@@ -4,7 +4,6 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.moviezone.dai_api.model.entity.Movie;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.http.*;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
@@ -53,7 +52,7 @@ public class MovieDAOImplementation implements IMovieDAO {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.add("accept", "application/json");
-        headers.add("Authorization",  Dotenv.load().get("TMDB_TOKEN")  );
+        headers.add("Authorization",  System.getenv("TMDB_TOKEN")  );
         HttpEntity<String> entity = new HttpEntity<String>(headers);
 
         ResponseEntity<String> response = restTemplate.exchange(API_URL,HttpMethod.GET, entity, String.class);
@@ -99,7 +98,7 @@ public class MovieDAOImplementation implements IMovieDAO {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.add("accept", "application/json");
-        headers.add("Authorization",  Dotenv.load().get("TMDB_TOKEN")  );
+        headers.add("Authorization",  System.getenv("TMDB_TOKEN")  );
         HttpEntity<String> entity = new HttpEntity<String>(headers);
 
         ResponseEntity<String> response = restTemplate.exchange(API_URL,HttpMethod.GET, entity, String.class);
