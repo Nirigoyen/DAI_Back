c9c690e65b33c92b381f794b7200d4618ab229a2
Máximo Fain <maximofain0@gmail.com>
2024-07-12 19:06:31 -0300

    Update FavMovie.java

--- src/main/java/com/moviezone/dai_api/model/entity/FavMovie.java
+++ src/main/java/com/moviezone/dai_api/model/entity/FavMovie.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "Favourites")
+@Table(name = "favourites")
 public class FavMovie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
